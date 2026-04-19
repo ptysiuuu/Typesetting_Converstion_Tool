c084771ac9d34a02233c81306db5ecebbb7bf723
Monika <mwjung10@gmail.com>
2026-04-19 15:41:57 +0200

    bindigs.cpp implemented

---
+++ bindings/bindings.cpp
@@ -0,0 +1,17 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h> 
+#include "dispatcher.h"
+
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(converter_bindings, m) {
+    m.doc() = "C++ core bindings for MarkdownConverter tool"; 
+   
+    m.def("convert_markdown", &convert_markdown, 
+          "Converts GFM Markdown string to LaTeX or Typst format",
+          py::arg("source_text"), 
+          py::arg("target_format"));
+
+    py::register_exception<std::runtime_error>(m, "ConverterError");
+}
