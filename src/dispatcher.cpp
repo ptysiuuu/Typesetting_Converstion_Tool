607f9efdf83c3eebc1a0310f647c9f4f55bf0f2e
Monika <mwjung10@gmail.com>
2026-04-19 15:51:52 +0200

    dispatcher. cpp first version (to be updated later)

---
+++ src/dispatcher.cpp
@@ -0,0 +1,30 @@
+
+// TODO:  still not implemented so maght be changed earlier
+#include "include/dispatcher.h"
+#include "include/parsers/gfm_parser.h"   
+#include "include/writers/latex_writer.h" 
+#include "include/writers/typst_writer.h" 
+#include <stdexcept>
+
+std::string convert_markdown(const std::string& source_text, const std::string& target_format) {
+    GFMParser parser;   //  TODO: TO BE CHANGED IN CASE THERE IS A DIFFERENT PARSER NAME 
+    auto document = parser.parse(source_text); 
+
+    if (!document) {
+        throw std::runtime_error("Failed to parse Markdown source.");
+    }
+
+    if (target_format == "latex") {
+        LaTeXWriter writer;
+        document->accept(writer); 
+        return writer.getOutput();
+    } 
+    else if (target_format == "typst") {
+        TypstWriter writer;
+        document->accept(writer);
+        return writer.getOutput();
+    } 
+    else {
+        throw std::invalid_argument("Unsupported target format: " + target_format);
+    }
+}
